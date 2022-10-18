23dc19a2f076ac326f3c0080b176f8b1ad3b0efc
JoWonHyeung <mesh153@naver.com>
2022-10-18 23:56:49 +0900

    ADD image rescaling code

--- app.py
+++ app.py
@@ -1,57 +1,85 @@
+import base64
+from typing import io
+
 from flask import Flask, request, render_template
 from keras.models import load_model
 from keras_preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
 from PIL import Image
 import numpy as np
-import pandas as pd
-import tensorflow as tf
-import os
-from scipy import misc
-
-pd.options.display.float_format = '{:.5f}'.format
+import flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False #response시, 한글 깨짐 이슈 해결
 
-#데이터 load
-model = load_model('model/keras_model.h5')
+label = {0:'고추탄저병',
+         1:'고추흰가루병',
+         2:'무검은무늬병',
+         3:'무노균병',
+         4:'배추검은썩음병',
+         5:'배추노균병',
+         6:'정상_고추',
+         7:'정상_무',
+         8:'정상_배추',
+         9:'정상_콩',
+         10:'정상_파',
+         11:'콩불마름병',
+         12:'콩점무늬병',
+         13:'파검은무늬병',
+         14:'파노균병',
+         15:'파녹병'}
 
 @app.route('/')
 @app.route("/test")
 def index():
-    return "Server Test"
+    return "server test"
 
 # 데이터 예측 처리
 @app.route('/prediction')
-def make_prediction():
-    data = {"success": False};
+def local_predict_test():
+    model = load_model("C:/Users/Jo/Al_Flask_API_Server/model/xception_epoch10_2.h5")
 
-    image = Image.open("test_image/배추노균병.png")
-    image = prepare_image(image, target=(224, 224))
+    image = Image.open("test_image/test_imag.jpeg")
+    processed_image = preprocess_image(image, target_size=(224, 224))
 
-    preds = model.predict(image)
-    return str(np.argmax(preds[0]))
+    prediction = model.predict(processed_image).tolist()
 
-    # if request.method == 'POST':
-    #     pass
+    response = {
+            'result': {
+                'crop_name': label[np.argmax(prediction[0])],
+                'percentage' : max(prediction[0])
+            }
+        }
+    return flask.jsonify(response)
 
-def prepare_image(image, target):
-    # image mode should be "RGB"
+def preprocess_image(image, target_size):
     if image.mode != "RGB":
-        image = image.convert("RGB");
-
-    # resize for model
-    image = image.resize(target)
+        image = image.convert("RGB")
+    image = image.resize(target_size)
     image = img_to_array(image)
+    image = image / 255
     image = np.expand_dims(image, axis=0)
-    image = imagenet_utils.preprocess_input(image)
-
-    # return it
     return image
+
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     message = request.get_json(force=True)
+#     encoded = message['image']
+#     decoded = base64.b64decode(encoded)
+#     image = Image.open(io.BytesIO(decoded))
+#     processed_image = preprocess_image(image, target_size=(224, 224))
+#
+#     prediction = model.predict(processed_image).tolist()
+#
+#     response = {
+#         'prediction': {
+#             'dog': prediction[0][0],
+#             'cat': prediction[0][1]
+#         }
+#     }
+#     return jsonify(response)
 
 if __name__ == '__main__':
 
     # Flask 서비스 스타트
-    app.run(port=8000)
     app.run(host='0.0.0.0',port=8000)
-    app.run(host='172.31.90.151',port=8000)
