0392fc386487ffeebdbfeed33c037b36337335c2
JoWonHyeung <mesh153@naver.com>
2022-10-21 16:56:07 +0000

    add script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+	echo 'building the applications...'
+}
+
+def testApp() {
+	echo 'testing the applications...'
+}
+
+def deployApp() {
+	echo 'deploying the applicaiton...'
+	echo "deploying version ${params.VERSION}"
+}
+return this
