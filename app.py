506bb6a70e1f8457262fb2e394834e336b6fce02
JoWonHyeung <mesh153@naver.com>
2022-11-15 23:31:28 +0900

    server test

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ label = {0:'고추탄저병',
 
 @app.route('/')
 def index():
-    return "capstone server test"
+    return "capstone server"
 
 def preprocess_image(image, target_size):
     if image.mode != "RGB":
@@ -43,9 +43,9 @@ def preprocess_image(image, target_size):
 @app.route("/prediction",methods=['POST'])
 def predict():
     #server model load
-    #model = load_model("/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
+    model = load_model("/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
     #local model load
-    model = load_model("C:/Users/Jo/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
+    #model = load_model("C:/Users/Jo/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
 
     #plantType = request.form['plantType']
 
@@ -64,60 +64,60 @@ def predict():
 
     return jsonify(response)
 
-@app.route("/prediction/test")
-def predictLayer2():
-    #server model load
-    model = load_model("/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5")
-    #local model load
-    #model = load_model("C:/Users/Jo/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
-
-    #사용자 입력 작물
-    plantType = request.form['plantType']
-
-    #image processing
-    image = request.files['image'].read()
-
-    image = Image.open(io.BytesIO(image))
-    processed_image = preprocess_image(image, target_size=(224, 224))
-
-    #predict
-    prediction = model.predict(processed_image).tolist()
-
-    #result label -> crop name
-    pestNameToCrop = label_to_crop(label[np.argmax(prediction[0])])
-    print("result label to crop name : " + pestNameToCrop)
-
-    # 입력 작물과 1차 layer를 거친 결과로 나온 작물이 다른 경우 작물별 학습 진행
-    if plantType != pestNameToCrop:
-        if plantType == "파":
-            gochu_model = load_model("/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5")
-            prediction = gochu_model.predict(processed_image).tolist()
-            response = {
-                'resultLayer2': {
-                    'pestName': label[np.argmax(prediction[0])],
-                    'pestPercentage': max(prediction[0])
-                }
-            }
-            return jsonify(response)
-        elif plantType == "배추":
-            pass
-        elif plantType == "콩":
-            pass
-        elif plantType == "고추":
-            pass
-        elif plantType == "무":
-            pass
-        else:
-            print("PlantType Error")
-
-    response = {
-                'result': {
-                    'pestName': label[np.argmax(prediction[0])],
-                    'pestPercentage' : max(prediction[0])
-                }
-            }
-
-    return jsonify(response)
+# @app.route("/prediction/test")
+# def predictLayer2():
+#     #server model load
+#     model = load_model("/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5")
+#     #local model load
+#     #model = load_model("C:/Users/Jo/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5")
+#
+#     #사용자 입력 작물
+#     plantType = request.form['plantType']
+#
+#     #image processing
+#     image = request.files['image'].read()
+#
+#     image = Image.open(io.BytesIO(image))
+#     processed_image = preprocess_image(image, target_size=(224, 224))
+#
+#     #predict
+#     prediction = model.predict(processed_image).tolist()
+#
+#     #result label -> crop name
+#     pestNameToCrop = label_to_crop(label[np.argmax(prediction[0])])
+#     print("result label to crop name : " + pestNameToCrop)
+#
+#     # 입력 작물과 1차 layer를 거친 결과로 나온 작물이 다른 경우 작물별 학습 진행
+#     if plantType != pestNameToCrop:
+#         if plantType == "파":
+#             gochu_model = load_model("/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5")
+#             prediction = gochu_model.predict(processed_image).tolist()
+#             response = {
+#                 'resultLayer2': {
+#                     'pestName': label[np.argmax(prediction[0])],
+#                     'pestPercentage': max(prediction[0])
+#                 }
+#             }
+#             return jsonify(response)
+#         elif plantType == "배추":
+#             pass
+#         elif plantType == "콩":
+#             pass
+#         elif plantType == "고추":
+#             pass
+#         elif plantType == "무":
+#             pass
+#         else:
+#             print("PlantType Error")
+#
+#     response = {
+#                 'result': {
+#                     'pestName': label[np.argmax(prediction[0])],
+#                     'pestPercentage' : max(prediction[0])
+#                 }
+#             }
+#
+#     return jsonify(response)
 
 def label_to_crop(crop):
     if crop == "고추흰가루병" or crop == "고추탄저병" or crop == "정상_고추": return "고추"
