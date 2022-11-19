2b860c803373a1aeb20303d66eb36116af8bd3eb
JoWonHyeung <mesh153@naver.com>
2022-11-20 01:13:40 +0900

    fix main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from keras.models import load_model
 from keras_preprocessing.image import img_to_array
 from PIL import Image
 from monitoring import instrumentator
+import numpy as np
 
 label = {0:'고추탄저병',
          1:'고추흰가루병',
