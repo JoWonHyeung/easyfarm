9e30e3a9258f298d6d757a25938d0a06b74b5ca3
JoWonHyeung <mesh153@naver.com>
2022-11-20 11:22:34 +0900

    test

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Header
 from io import BytesIO
-from PIL import Image
 from keras.models import load_model
 from keras_preprocessing.image import img_to_array
 from PIL import Image
-from .monitoring import instrumentator
+from monitoring import instrumentator
+from typing import Union
 import numpy as np
 
-label = {0:'고추탄저병',
-         1:'고추흰가루병',
-         2:'무검은무늬병',
-         3:'무노균병',
-         4:'배추검은썩음병',
-         5:'배추노균병',
-         6:'정상_고추',
-         7:'정상_무',
-         8:'정상_배추',
-         9:'정상_콩',
-         10:'정상_파',
-         11:'콩불마름병',
-         12:'콩점무늬병',
-         13:'파검은무늬병',
-         14:'파노균병',
-         15:'파녹병'}
+label = {0: '고추탄저병',
+         1: '고추흰가루병',
+         2: '무검은무늬병',
+         3: '무노균병',
+         4: '배추검은썩음병',
+         5: '배추노균병',
+         6: '정상_고추',
+         7: '정상_무',
+         8: '정상_배추',
+         9: '정상_콩',
+         10: '정상_파',
+         11: '콩불마름병',
+         12: '콩점무늬병',
+         13: '파검은무늬병',
+         14: '파노균병',
+         15: '파녹병'}
 
-#server model path
+# server model path
 model_path = "/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5"
 model = load_model(model_path)
 
@@ -34,15 +34,15 @@ instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=
 
 @app.get('/')
 def root_route():
-    return {"error": "use GET /prediction instead of root route"}
+    return {"error": "capstone server"}
 
 # 데이터 준비
 @app.post('/prediction')
 async def prediction_route(file: UploadFile = File(...)):
     contents = await file.read()
-    image = Image.open(BytesIO(contents))
+    img = Image.open(BytesIO(contents))
 
-    processed_image = preprocess_image(image, target_size=(224, 224))
+    processed_image = preprocess_image(img, target_size=(224, 224))
     prediction = model.predict(processed_image).tolist()
 
     response = {
@@ -52,6 +52,7 @@ async def prediction_route(file: UploadFile = File(...)):
         }
     }
     return response
+
 
 def preprocess_image(image, target_size):
     if image.mode != "RGB":
