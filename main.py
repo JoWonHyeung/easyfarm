f4d8de18bf6d34233e567241f70af8b0c711e935
JoWonHyeung <mesh153@naver.com>
2022-11-27 23:29:22 +0900

    response header error about encoding

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ async def prediction_route(image: UploadFile = File(...), response: Response = N
             'pestPercentage': max(prediction[0])
         }
     }
-    response.headers['pestName'] = label[np.argmax(prediction[0])].encode('utf-8')
+    response.headers['pestName'] = np.argmax(prediction[0])
     return result
 
 
