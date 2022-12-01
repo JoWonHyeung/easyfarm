9210b52496ec0f52e94ab7af3b4c6aea7e10e07b
JoWonHyeung <mesh153@naver.com>
2022-12-02 01:11:46 +0900

    montoring test

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ from pydantic import BaseModel, Field
 
 def model_load():
     # server model path
-    # model_path = "/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5"
-    # fa_path = "/Al_Flask_API_Server/model/fa_xception_unfreeze.h5"
-    # gochu_path = "/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5"
-    # kong_path = "/Al_Flask_API_Server/model/kong_xception_unfreeze.h5"
-    # mu_path = "/Al_Flask_API_Server/model/mu_xception_fine_tuning.h5"
-    # bachu_path = "/Al_Flask_API_Server/model/bachu_xception_fine_tuning.h5"
+    model_path = "/Al_Flask_API_Server/model/xception_epoch10_fine_tuning.h5"
+    fa_path = "/Al_Flask_API_Server/model/fa_xception_unfreeze.h5"
+    gochu_path = "/Al_Flask_API_Server/model/gochu_xception_unfreeze.h5"
+    kong_path = "/Al_Flask_API_Server/model/kong_xception_unfreeze.h5"
+    mu_path = "/Al_Flask_API_Server/model/mu_xception_fine_tuning.h5"
+    bachu_path = "/Al_Flask_API_Server/model/bachu_xception_fine_tuning.h5"
 
     # local model path
-    model_path = "C:/Users/Jo/capstone/model/xception_epoch10_fine_tuning.h5"
-    fa_path = "C:/Users/Jo/capstone/model/fa_xception_unfreeze.h5"
-    gochu_path = "C:/Users/Jo/capstone/model/gochu_xception_unfreeze.h5"
-    kong_path = "C:/Users/Jo/capstone/model/kong_xception_unfreeze.h5"
-    mu_path = "C:/Users/Jo/capstone/model/mu_xception_fine_tuning.h5"
-    bachu_path = "C:/Users/Jo/capstone/model/bachu_xception_fine_tuning.h5"
+    # model_path = "C:/Users/Jo/capstone/model/xception_epoch10_fine_tuning.h5"
+    # fa_path = "C:/Users/Jo/capstone/model/fa_xception_unfreeze.h5"
+    # gochu_path = "C:/Users/Jo/capstone/model/gochu_xception_unfreeze.h5"
+    # kong_path = "C:/Users/Jo/capstone/model/kong_xception_unfreeze.h5"
+    # mu_path = "C:/Users/Jo/capstone/model/mu_xception_fine_tuning.h5"
+    # bachu_path = "C:/Users/Jo/capstone/model/bachu_xception_fine_tuning.h5"
 
     model = load_model(model_path)
     fa_model = load_model(fa_path)
